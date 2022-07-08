e725a1de2dce1c6e5c0e09c8c23fbc792d6fc551
Damien Clarke <dclarke@atlassian.com>
2022-07-08 16:14:19 +1000

    fix param type

--- src/internal/construct-node.ts
+++ src/internal/construct-node.ts
@@ -1,12 +1,17 @@
-import { Graph, isFaustGraph, GraphAudioNode } from "../graph";
+import {
+  Graph,
+  isFaustGraph,
+  GraphAudioNode,
+  ParamValueObject,
+} from "../graph";
 import { constructFaustNode } from "./construct-faust-node";
 
-export async function constructNode<P>(
+export async function constructNode<P extends ParamValueObject>(
   audioContext: AudioContext,
   graph: Graph
 ): Promise<GraphAudioNode<P>> {
   if (isFaustGraph(graph)) {
-    return await constructFaustNode(audioContext, graph);
+    return await constructFaustNode<P>(audioContext, graph);
   }
   // @ts-expect-error - types do not allow this path yet
   throw new Error(`graph has invalid type "${graph.type}"`);
