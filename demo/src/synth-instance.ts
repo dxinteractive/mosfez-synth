fb0aae004d06340a244a0e5cc670612a9147a1c0
Damien Clarke <dclarke@atlassian.com>
2022-07-27 18:11:36 +1000

    pass voices from demo

--- demo/src/synth-instance.ts
+++ demo/src/synth-instance.ts
@@ -1,4 +1,5 @@
 import { faust } from "mosfez-synth/faust";
+import { poly } from "mosfez-synth/poly";
 import { Synth } from "mosfez-synth/synth";
 import { touchStart } from "mosfez-synth/touch-start";
 
@@ -7,34 +8,37 @@ const audioContext = new window.AudioContext();
 touchStart(audioContext);
 
 type Params = {
+  voice: string;
   force: number;
   pitch: number;
+  speed: number;
 };
 
 // create synth
 const synth = new Synth<Params>({ audioContext });
 
 // create custom oscillator dsps
-const triangle = faust(
+const triangle = faust<Params>(
   "process = os.triangle(params.pitch : si.smooth(0.9) : ba.midikey2hz);",
-  [],
   {
     pitch: ":pitch",
   }
 );
 
 // create custom gate
-const gated = faust("process = *(params.force : si.smooth(0.9));", [triangle], {
+const gated = faust<Params>("process = *(params.force : si.smooth(0.99999));", {
+  inputs: [triangle],
   force: ":force",
 });
 
+const polyphonic = poly<Params>(gated, 4);
+
 // create custom tremolo dsp
-const completeDsp = faust(
+const completeDsp = faust<Params>(
   "process = *(os.osc(params.speed) * 0.1 + 0.8) : *(0.2);",
-  [gated],
   {
+    inputs: [polyphonic],
     speed: 8,
-    force: ":force",
   }
 );
 
