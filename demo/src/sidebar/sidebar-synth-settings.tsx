e299b548f7a3ae2cf68553d2b744925dd72a7d3d
Damien Clarke <dclarke@atlassian.com>
2022-08-07 19:20:07 +1000

    add synth description

--- demo/src/sidebar/sidebar-synth-settings.tsx
+++ demo/src/sidebar/sidebar-synth-settings.tsx
@@ -18,6 +18,17 @@ export function SidebarSynthSettings() {
           );
         })}
       </FieldRow>
+      More synth options will be added soon.
+      <br />
+      <br />
+      Right now we have a triangle with infinitely fast attack and long release
+      (and bad clicking on note start), with up to 8 voices, each voice randomly
+      panned on note start, through a tremolo effect that changes speed
+      occasionally.
+      <br />
+      <br />
+      We also have a couple of faust compilation problems, and some sub-par
+      behaviour with force controls on iPhones.
     </div>
   );
 }
